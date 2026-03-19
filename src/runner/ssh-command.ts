f24503877ec294e474570f8fc33aabece156761f
Jess Martin <jessmartin@gmail.com>
2026-03-19 04:41:50 -0400

    Format SSH remote command helper

--- src/runner/ssh-command.ts
+++ src/runner/ssh-command.ts
@@ -3,4 +3,3 @@ import { quoteShellToken } from "./local-command.js";
 export function buildSshRemoteCommand(args: readonly string[]): string {
   return args.map((arg) => quoteShellToken(arg)).join(" ");
 }
-
